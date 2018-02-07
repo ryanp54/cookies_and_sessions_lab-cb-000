94ae213a7b8f8afea8048066bde208a742344959
5-4 <github email address>
2018-02-06 22:12:15 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def cart
+  	session[:cart] ||= []
+  end
+
+  def home
+  	
+  end
+
+  def add
+  	cart << params[:product]
+  	redirect_to index_path
+  end
+
+  def index
+  	render '/products/index'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
+  get '/' => 'application#home', as: :home
+  post '/' => 'application#add', as: :add
+  get '/products/index' => 'application#index', as: :index
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
